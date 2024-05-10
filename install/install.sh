358717e21ac7d83c223d80cdd044848fb13a3f7e
alexpswanson <alexpswanson@gmail.com>
2024-05-10 15:22:03 +0300

    moving omz install to after brew install

--- install/install.sh
+++ install/install.sh
@@ -2,11 +2,11 @@
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
-# Install oh-my-zsh
-. "$DOTFILES_DIR/install/oh-my-zsh.sh"
-
 # Install brew with packages & casks, composer and yarn global packages
 . "$DOTFILES_DIR/install/brew.sh"
+
+# Install oh-my-zsh
+. "$DOTFILES_DIR/install/oh-my-zsh.sh"
 
 # Bunch of symlinks
 . "$DOTFILES_DIR/install/symlinks.sh"
